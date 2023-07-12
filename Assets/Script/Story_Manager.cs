e1882c8f70bb7f7eeb471404bdce2c554f5c23de
Ahnjaemin1208 <michael1208@naver.com>
2023-07-13 06:49:06 +0900

    7/13 수정
    기본 파일 설정

---
+++ Assets/Script/Story_Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Story_Manager : MonoBehaviour
+{
+    public Text TalkText;
+    public GameObject scanObject;
+
+    public void Update()
+    {
+
+    }
+
+}
