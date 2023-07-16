16b96bb67e3873470c448c09346e4c7ab37cc3b9
Ahnjaemin1208 <michael1208@naver.com>
2023-07-16 13:00:14 +0900

    7/16
    Feat: PlayerMove, Resolution

---
+++ Assets/Script/Player_Action.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigid;
+    float h;
+    float v;
+    bool isHorizonMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();   
+    }
+
+    void Update()
+    {
+        //Move value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //Check Horizontal Move 
+        if (hDown) isHorizonMove= true;
+        else if(vDown) isHorizonMove= false;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = new Vector2(h, v) * speed;
+    }
+}
