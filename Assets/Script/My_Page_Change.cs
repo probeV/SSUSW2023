f608628583b064ea8ef7dc5f6232eb2ad64aa90a
ywoo121 <ywoo121@naver.com>
2023-07-20 03:25:35 +0900

    7/19
    메인화면/마이페이지/세팅

---
+++ Assets/Script/My_Page_Change.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class My_Page_Change : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("MyPage");
+    }
+}
