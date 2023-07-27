90160c956e78b2c3fd0d29d7e1c7cbc5b3da4caa
Yarrly <74090200+eunsoA@users.noreply.github.com>
2023-07-27 17:22:45 +0900

    update mission_clue_key

---
+++ Assets/Script/Mission_clue_key_touchpad.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission_clue_key_touchpad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello unity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
