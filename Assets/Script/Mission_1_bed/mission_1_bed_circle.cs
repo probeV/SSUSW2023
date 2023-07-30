9cfe10e4c5b4b9cd3286d2b6f14346611027dddb
Yarrly <74090200+eunsoA@users.noreply.github.com>
2023-07-30 17:04:31 +0900

    mission_1_bed edit & move sound folder

--- Assets/Script/Mission_1_bed/mission_1_bed_circle.cs
+++ Assets/Script/Mission_1_bed/mission_1_bed_circle.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class mission_1_bed_circle : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("circle script start");
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        //if(Input.GetButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) 
         {
             Debug.Log("circle check");
+            audioSource.Play();
         }
+    }
+    private void OnMouseDown() //마우스 버튼
+    {
+        Debug.Log("circle check");
+        audioSource.Play();
     }
 }
