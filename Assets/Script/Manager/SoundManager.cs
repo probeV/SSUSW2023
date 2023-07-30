14aadee95a2817038fec0e86cd30c2499a432bae
안재민 <michael1208@naver.com>
2023-07-30 21:03:57 +0900

    7/30

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public bool isNaviSound=true;
+    public bool isWalking=false;
+}
