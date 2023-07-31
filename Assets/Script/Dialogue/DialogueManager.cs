fc15551e25362f85c1aa9c08ffae1b3fd15e22e4
ywoo121 <ywoo121@naver.com>
2023-08-01 00:43:29 +0900

    스토리 콘솔에 로그 찍히게

--- Assets/Script/Dialogue/DialogueManager.cs
+++ Assets/Script/Dialogue/DialogueManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject go_DialogueBar;
+    [SerializeField] TextMeshProUGUI txt_Dialogue;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
