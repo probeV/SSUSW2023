922cf4762396dcceb29c996cc67897e08c3fe903
ywoo121 <ywoo121@naver.com>
2023-07-31 15:17:32 +0900

    csv파일 로그에 찍기

---
+++ Assets/Script/Manager/DatabaseManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    public static DatabaseManager instance;
+
+    [SerializeField] string csv_FileName;
+
+    Dictionary<int, Dialogue> dialogueDic = new Dictionary<int, Dialogue>();
+
+    public static bool isFinish = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DialogueParser theParser = GetComponent<DialogueParser>();
+            Dialogue[] dialogues = theParser.Parse(csv_FileName);
+            for (int i = 0; i < dialogues.Length; i++)
+            {
+                dialogueDic.Add(i + 1, dialogues[i]);
+            }
+            isFinish = true;
+        }
+    }
+
+
+    public Dialogue[] GetDialouge(int _StartNum, int _EndNum)
+    {
+        List<Dialogue> dialogueList = new List<Dialogue>();
+
+        for (int i = 0; i <= _EndNum - _StartNum; i++)
+        {
+            dialogueList.Add(dialogueDic[_StartNum + i]);
+        }
+
+        return dialogueList.ToArray();
+    }
+}
